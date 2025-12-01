9461efac661472cfe635317072cddfd5844b9423
anyusernameisokay <n2o_ju@gmx.de>
2025-12-01 19:23:10 +0100

    Setting up tests

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+
+
+try:
+    with open("README.md", "r", encoding="utf-8") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = ""
+
 
 setup(
     name='acld',
